92b84ae7bed49389b4a71c4247ceb65b23753937
Cole Atkins <cole.atkins.005@gmail.com>
2026-01-26 10:01:56 -0600

    Started making the pygame chess

---
+++ Chess/ChessEngine.py
@@ -0,0 +1,52 @@
+"""
+Current State of the Chess Game
+Determine Valid Moves at game state
+Keep move log
+"""
+
+class GameState():
+    def __init__(self):
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
+
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move)
+        self.whiteToMove = not self.whiteToMove
+
+class Move():
+
+    ranksToRows = {"1":7, "2":6, "3":5, "4":4,
+                   "5":3, "6":2, "7":1, "8":0}
+    rowsToRanks = {7:"1", 6:"2", 5:"3", 4:"4",
+                   3:"5", 2:"6", 1:"7", 0:"8"}
+    filesToCols = {"a":0, "b":1, "c":2, "d":3,
+                   "e":4, "f":5, "g":6, "h":7}
+    colsToFiles = {0:"a", 1:"b", 2:"c", 3:"d",
+                   4:"e", 5:"f", 6:"g", 7:"h"}
+
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+    
+    def getRankFile(self, r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r]
